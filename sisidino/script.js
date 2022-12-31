30b89b46544e9fc700863a01f26d8eb772c7cb17
Weinan Feng <weinanfeng@Weinans-MacBook-Air.local>
2022-12-31 16:42:05 +0800

    add youchen sisi game

---
+++ sisidino/script.js
@@ -0,0 +1,80 @@
+                                                                                                                                                                                                                       var character = document.getElementById('character')
+var block = document.getElementById('block')
+
+var score = 0
+
+console.log(character)
+function jump() {
+	console.log('jump')
+	if (character.classList != 'animate'){
+		character.classList.add('animate')	
+	}
+
+	setTimeout(function(){
+		character.classList.remove('animate')
+	}, 500)
+} 
+
+var isGameOver = false
+var checkDeak = setInterval(function() {
+	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+	var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+	if(blockLeft < 20 && blockLeft > 0 && characterTop >=100){
+		block.style.animation = 'none';
+		block.style.display = 'none';
+		isGameOver = true;
+		
+		var gameover = document.getElementById('gameover');
+		gameover.innerHTML = 'Game Over - You Die!'
+	}
+	if (!isGameOver) {
+		score = score + 10	
+	}
+	
+	document.getElementById('score').innerHTML = "  " + parseInt(score / 100)
+}, 10)	
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        jump()
+    }
+}
+
+var restart = function() {
+	console.log('Restart..')
+	setTimeout("location.reload(true);", 10)	
+}
+
+
+
+
+
+
+window.onload = function() {
+	setInterval(checkBgm,100);
+}
+
+var checkBgm = function() {
+	var music = document.getElementById('bgm');
+	if (music.paused) {
+		music.paused = false;
+		music.play()
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
