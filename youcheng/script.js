6f54fd8eab758f0584a2f87d9a4d343e0b236e41
Weinan Feng <weinanfeng@Weinans-MacBook-Air.local>
2023-03-22 17:53:54 +0800

    update youcheng

--- youcheng/script.js
+++ youcheng/script.js
@@ -13,22 +13,34 @@ var block = document.getElementById('block')
  }
 
 var points = 0;
+
+var gameOver = false;
+
 var checkDead = function(){
     var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
 
-    if (blockLeft < 20 && blockLeft > 0 && playerTop >= -300){
+    if (blockLeft < 20 && blockLeft > 0 && playerTop >= -280){
         block.style.animation = 'none';
         block.style.display = 'none';
-        alert('绊倒了!')
-    }else if (blockLeft < 20 && blockLeft > 0){
+        gameOver = true;
+
+        var gameTitle = document.getElementById('gameoverTitle')
+        gameTitle.style.visibility = "visible";
+
+    }
+}
+
+var updatePoints =function () {
+    if(!gameOver){
         points = points + 1;
         var scoreBar = document.getElementById('score')
         scoreBar.innerHTML = points
     }
 }
 
-setInterval(checkDead, 10)
+setInterval(updatePoints, 2000)
+setInterval(checkDead, 20)
 
 window.onload = function(){
     console.log('onload...');
